a2668fd7b5db50adc827606059ec51d6a43ef7aa
brooklyneti2012 <ac106494@avcol.school.nz>
2022-10-21 09:54:36 +1300

    deleted all the back end components for write up purposes

--- AcUniform/Pages/JuniorStudents.cshtml.cs
+++ AcUniform/Pages/JuniorStudents.cshtml.cs
@@ -1,20 +1,20 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-//namespace AcUniform.Pages
-//{
-//    public class JuniorStudentsModel : PageModel
-//    {
-//        private readonly ILogger<JuniorStudentsModel> _logger;
+namespace AcUniform.Pages
+{
+    public class JuniorStudentsModel : PageModel
+    {
+        private readonly ILogger<JuniorStudentsModel> _logger;
 
-//        public JuniorStudentsModel(ILogger<JuniorStudentsModel> logger)
-//        {
-//            _logger = logger;
-//        }
+        public JuniorStudentsModel(ILogger<JuniorStudentsModel> logger)
+        {
+            _logger = logger;
+        }
 
-//        public void OnGet()
-//        {
+        public void OnGet()
+        {
 
-//        }
-//    }
-//}
+        }
+    }
+}
