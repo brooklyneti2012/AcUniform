87e3eb5832285ec3bdc69b0e8618b7f88f56084e
Brooklyn Eti <ac106494@avcol.school.nz>
2022-10-27 10:23:34 +1300

    .

---
+++ AcUniform/Models/Junior.cs
@@ -0,0 +1,9 @@
+﻿namespace AcUniform.Models
+{
+    public class Junior
+    {
+        public int JuniorID { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
